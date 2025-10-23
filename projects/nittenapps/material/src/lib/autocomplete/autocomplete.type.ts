5e843f12bf6fd4d241256b21ddc166adf3905b2f
Sergio Del Valle <sergio@nittenapps.dev>
2025-10-23 16:34:37 -0600

    fix: better autocomplete behavior

--- projects/nittenapps/material/src/lib/autocomplete/autocomplete.type.ts
+++ projects/nittenapps/material/src/lib/autocomplete/autocomplete.type.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, Type } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, Type } from '@angular/core';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 import { FieldTypeConfig, StackFieldConfig } from '@nittenapps/forms';
 import { StackFieldSelectProps } from '@nittenapps/forms/select';
-import { map, Observable, of, startWith, switchMap, tap } from 'rxjs';
+import { debounceTime, map, Observable, of, Subject, switchMap } from 'rxjs';
+
 import { FieldType, StackFieldProps } from '../form-field';
 
 interface AutocompleteProps extends StackFieldProps, StackFieldSelectProps {
@@ -11,6 +12,7 @@ interface AutocompleteProps extends StackFieldProps, StackFieldSelectProps {
   disableOptionCentering?: boolean;
   typeaheadDebounceInterval?: number;
   displayWith: ((value: any) => string) | null;
+  filterMethod?: (field: StackFieldConfig, term: string) => Observable<any[]>;
   panelClass?: string;
 }
 
@@ -21,9 +23,12 @@ export interface StackAutocompleteFieldConfig extends StackFieldConfig<Autocompl
 @Component({
   selector: 'nas-field-mat-autocomplete',
   templateUrl: './autocomplete.type.html',
+  changeDetection: ChangeDetectionStrategy.Default,
 })
 export class StackFieldAutocomplete extends FieldType<FieldTypeConfig<AutocompleteProps>> implements OnInit {
   filteredOptions?: Observable<any[]>;
+
+  private searchSubject = new Subject<string>();
 
   override defaultOptions = {
     props: {
@@ -34,18 +39,32 @@ export class StackFieldAutocomplete extends FieldType<FieldTypeConfig<Autocomple
   };
 
   ngOnInit(): void {
-    this.filteredOptions = this.formControl.valueChanges.pipe(
-      startWith(''),
+    this.filteredOptions = this.searchSubject.pipe(
+      debounceTime(this.props.typeaheadDebounceInterval || 300),
       switchMap((term) => this.filterOptions(term))
     );
+    this.formControl.valueChanges.subscribe((value) => {
+      if (typeof value !== 'string') {
+        if (!value) {
+          this.searchSubject.next('');
+        }
+      }
+    });
+  }
+
+  onKeyUp(event: KeyboardEvent): void {
+    if (this.props.readonly) {
+      return event.preventDefault();
+    }
+    this.searchSubject.next((event.target! as HTMLInputElement).value);
   }
 
   optionSelected($event: MatAutocompleteSelectedEvent): void {
-    this.props.change?.(this.field, $event);
+    this.props.change?.(this.field, $event.option);
   }
 
   private filterOptions(term: string): Observable<any[]> {
-    if (!this.props.options) {
+    if (!this.props.options && !this.props.filterMethod) {
       return of([]);
     }
 
@@ -53,10 +72,13 @@ export class StackFieldAutocomplete extends FieldType<FieldTypeConfig<Autocomple
       if (this.props.options instanceof Observable) {
         return this.props.options;
       }
-      return of(this.props.options);
+      return of(this.props.options || []);
     }
 
     term = term.toLowerCase();
+    if (this.props.filterMethod) {
+      return this.props.filterMethod(this.field, term);
+    }
     if (this.props.options instanceof Observable) {
       return this.props.options.pipe(
         map((options: any[]) =>
@@ -67,7 +89,7 @@ export class StackFieldAutocomplete extends FieldType<FieldTypeConfig<Autocomple
       );
     }
     return of(
-      this.props.options.filter(
+      this.props.options!.filter(
         (option) => option.code?.toLowerCase().includes(term) || option.name?.toLowerCase().includes(term)
       )
     );
