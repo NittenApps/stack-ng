61fa2286671550de79a6875f97dc220df54ea0c0
Sergio Del Valle <sergio@nittenapps.dev>
2026-02-25 08:54:29 -0600

    fix: update select option tracking for improved performance

--- projects/nittenapps/api/src/lib/services/config.service.ts
+++ projects/nittenapps/api/src/lib/services/config.service.ts
@@ -1,11 +1,14 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Catalog, CatalogValue } from '@nittenapps/common';
+import { AttributeValue, Catalog, CatalogValue } from '@nittenapps/common';
 import { map, Observable } from 'rxjs';
 
-import { ApiConfig, ApiResponse, ListBody } from '../types';
+import { ApiConfig, ApiResponse, ListBody, ObjectBody } from '../types';
 
 export class ConfigService {
-  constructor(private config: ApiConfig, private http: HttpClient) {}
+  constructor(
+    private config: ApiConfig,
+    private http: HttpClient,
+  ) {}
 
   getCatalogValue(catalogCode: string, code: string): Observable<CatalogValue> {
     return this.http
@@ -17,20 +20,20 @@ export class ConfigService {
 
   getCatalogValues(
     catalogCode: string,
-    params?: HttpParams | { [param: string]: string | number | boolean | ReadonlyArray<string | number | boolean> }
+    params?: HttpParams | { [param: string]: string | number | boolean | ReadonlyArray<string | number | boolean> },
   ): Observable<CatalogValue[]> {
     return this.http
       .get<ApiResponse<CatalogValue, ListBody<CatalogValue>>>(
         `${this.config.baseUrl}/config/v1/catalog-values/${catalogCode}`,
         {
           params,
-        }
+        },
       )
       .pipe(map((response) => response.body.items));
   }
 
   getCatalogs(
-    params?: HttpParams | { [param: string]: string | number | boolean | ReadonlyArray<string | number | boolean> }
+    params?: HttpParams | { [param: string]: string | number | boolean | ReadonlyArray<string | number | boolean> },
   ): Observable<Catalog[]> {
     return this.http
       .get<ApiResponse<Catalog, ListBody<Catalog>>>(`${this.config.baseUrl}/config/v1/catalogs`, {
--- projects/nittenapps/material/src/lib/select/module.ts
+++ projects/nittenapps/material/src/lib/select/module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { StackFieldMatSelect } from './select.type';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatPseudoCheckboxModule } from '@angular/material/core';
 import { CommonModule as NASCommonModule } from '@nittenapps/common';
 import { StackFormsModule } from '@nittenapps/forms';
 import { StackFormsSelectModule } from '@nittenapps/forms/select';
 import { StackMatFormFieldModule } from '../form-field';
-import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [StackFieldMatSelect],
