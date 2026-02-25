11ec528c6431edccbf8baabf9feed393c5f8e751
Sergio Del Valle <sergio@nittenapps.dev>
2026-02-25 08:49:25 -0600

    refactor: update auth services to new angular directives

--- projects/nittenapps/auth/src/lib/services/auth.service.ts
+++ projects/nittenapps/auth/src/lib/services/auth.service.ts
@@ -1,42 +1,44 @@
-import { Injectable, Optional } from '@angular/core';
-import { KeycloakService } from 'keycloak-angular';
-import { KeycloakProfile } from 'keycloak-js';
+import { effect, inject, Injectable, signal } from '@angular/core';
+import { KEYCLOAK_EVENT_SIGNAL, KeycloakEventType } from 'keycloak-angular';
+import Keycloak from 'keycloak-js';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  constructor(@Optional() private keycloak?: KeycloakService) {}
+  isAuthenticated = signal(false);
+  profile = signal<Keycloak.KeycloakProfile | null>(null);
 
-  get profile(): Promise<KeycloakProfile | null> {
-    if (this.keycloak?.isLoggedIn()) {
-      return this.keycloak.loadUserProfile();
-    }
-    return Promise.resolve(null);
+  private readonly keycloak = inject(Keycloak);
+  private readonly keycloakSignal = inject(KEYCLOAK_EVENT_SIGNAL);
+
+  constructor() {
+    effect(() => {
+      const event = this.keycloakSignal();
+
+      if (event.type === KeycloakEventType.Ready || event.type === KeycloakEventType.AuthSuccess) {
+        this.keycloak.loadUserProfile().then((profile) => {
+          this.isAuthenticated.set(!!this.keycloak.authenticated);
+          this.profile.set(profile);
+        });
+      } else if (event.type === KeycloakEventType.AuthLogout) {
+        this.isAuthenticated.set(false);
+        this.profile.set(null);
+      }
+    });
   }
 
   get username(): string {
-    return this.keycloak?.getUsername() || 'Usuario';
+    return this.profile()?.username || 'unknown';
   }
 
   getUserRoles(): string[] {
-    return this.keycloak?.getUserRoles(true) || [];
-  }
-
-  isLoggedIn(): boolean {
-    return this.keycloak?.isLoggedIn() || false;
+    return this.keycloak.realmAccess?.roles || [];
   }
 
   login(redirectUri?: string): Promise<void> {
-    if (this.keycloak) {
-      const uri = window.location.origin + (redirectUri || '');
-      return this.keycloak.login({ redirectUri: uri });
-    }
-    return Promise.resolve();
+    return this.keycloak.login({ redirectUri: window.location.origin + (redirectUri || '') });
   }
 
   logout(redirectUri?: string): void {
-    if (this.keycloak) {
-      const uri = window.location.origin + (redirectUri || '');
-      this.keycloak.logout(uri);
-    }
+    this.keycloak.logout({ redirectUri: window.location.origin + (redirectUri || '') });
   }
 }
